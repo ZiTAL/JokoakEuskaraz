76c052b2110ecdb8e82640b25387aba6f0bb95f4
Sergio Muriel <sergio.muriel@boardriders.com>
2024-11-24 11:42:04 +0100

    Pid - eguneratzea

--- Pid/translate.php
+++ Pid/translate.php
@@ -43,7 +43,7 @@ foreach($file_lines as $file_line)
 
             $row[3] = preg_replace("/\n/"," ",$row[3]);
             $translation = system('curl -s \'https://api.euskadi.eus/itzuli/es2eu/v2/translate\' -X POST -H \'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0\' -H \'Accept: application/json, text/javascript, */*; q=0.01\' -H \'Accept-Language: eu,es;q=0.8,fr;q=0.5,en-US;q=0.3\' -H \'Accept-Encoding: gzip, deflate, br, zstd\' -H \'Content-Type: application/json\' -H \'Origin: https://www.euskadi.eus\' -H \'DNT: 1\' -H \'Connection: keep-alive\' -H \'Referer: https://www.euskadi.eus/\' -H \'Sec-Fetch-Dest: empty\' -H \'Sec-Fetch-Mode: cors\' -H \'Sec-Fetch-Site: same-site\' -H \'Pragma: no-cache\' -H \'Cache-Control: no-cache\' --data-raw \'{"mkey":"8d9016025eb0a44215c7f69c2e10861d","text":'.json_encode($row[3]).',"model":"generic_es2eu"}\' | jq -r ".translation"');
-            $translation = join("\n",preg_split("/.{0,70}\K(?:\s+|$)/", $translation, 0, PREG_SPLIT_NO_EMPTY));
+            $translation = join("\n",preg_split("/.{0,55}\K(?:\s+|$)/", $translation, 0, PREG_SPLIT_NO_EMPTY));
 
             print $row[3]." : $translation\n";
             $row[3] = preg_replace("/\n/","\\n",$translation);
