83abf7231a17259cf4a7d47e9fa5f78fd7324e93
bilbao_iban <bilbao_iban@eitb.eus>
2025-05-12 12:33:48 +0200

    MES konpilatuak

--- Silent Hill 2 Enhanced Edition/tools/build.php
+++ Silent Hill 2 Enhanced Edition/tools/build.php
@@ -1,32 +1,25 @@
 <?php
-$dir   = 'C:\projects\JokoakEuskaraz\Silent Hill 2 Enhanced Edition\lang\etc\message';
-$dir   = preg_replace("/\\\/", '/', $dir);
 
+$dir   = "../lang/etc/message/";
 $files = scandir($dir);
+
 $files = array_filter($files, function($file)
 {
-    if(preg_match("/\.eu\.txt$/", $file))
-        return true;
+    if(preg_match("/\.mes$/", $file) || preg_match("/\.es\.txt$/", $file))
+        return false;
+    return true;
 });
+
 $files = array_map(function($file) use ($dir)
 {
-    return "{$dir}/{$file}";
+    return "{$dir}{$file}";
 }, $files);
-/*
+
+
 foreach($files as $file)
 {
-    $command = "sh2msg.exe \"{$file}\"";
+    $command = "./sh2msg {$file}";
     echo "{$command}\n";
     shell_exec($command);
 }
-*/
-foreach($files as $file)
-{
-    $eu_mes  = preg_replace("/\.txt$/", '.mes', $file);
-    $mes     = preg_replace("/\.eu\.mes$/", '.mes', $eu_mes);
 
-    $command = "mv \"{$eu_mes}\" \"{$mes}\"";
-
-    echo "{$command}\n\n";
-    shell_exec($command);
-}
