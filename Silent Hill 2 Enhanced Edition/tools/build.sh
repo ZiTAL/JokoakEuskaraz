f9c31e895eab46a008cd6f0efab3f614dc7f2b90
zital <zital@riseup.net>
2025-09-20 10:10:48 +0200

    build sh

---
+++ Silent Hill 2 Enhanced Edition/tools/build.sh
@@ -0,0 +1,4 @@
+./sh2msg "/home/projects/JokoakEuskaraz/Silent Hill 2 Enhanced Edition/lang/etc/message/item_x_msg_s.txt"
+./sh2msg "/home/projects/JokoakEuskaraz/Silent Hill 2 Enhanced Edition/lang/etc/message/memo_x_msg_s.txt"
+./sh2msg "/home/projects/JokoakEuskaraz/Silent Hill 2 Enhanced Edition/lang/etc/message/stage_x_mansion_1f_msg_s.txt"
+
